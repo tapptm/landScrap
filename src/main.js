0f33fad96cc078e55c6019d8b002f9b9408207f2
tapptm <phuwadet00123@gmail.com>
2023-11-01 12:45:09 +0700

    update cors

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const logger = require("./api/config/winston");
+const cors = require('cors')
 const app = express();
 
 function startServer(app) {
+  app.use(cors())
   require("./api/routes")(app);
   require("./api/middleware/handleError")(app);
   app.listen(6688, () => logger.info("App Listening on port 6688"));
